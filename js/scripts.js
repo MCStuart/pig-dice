c311e18a560f22683c11a335ea87d4604b2808a7
Lindsey Baker and Stuart McKay <lindsey@>
2019-04-10 13:46:31 -0700

    Add player name input to PlayerInGame

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,22 @@ function PlayerInGame(name, total, turnTotal) {
   this.turnTotal = turnTotal
 }
 
-// Add roll value to current turn rollTotal
+// Assigns each new player a unique Id
+Game.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+// Adds new players to the Game
+Game.prototype.addPlayer = function(playerInGame) {
+  playerInGame.id = this.assignId();
+  this.players.push(playerInGame);
+}
+
+// funtion to sum rolls per turn resetting if one is rolled
 
 var turn = 0;
-
-// something goes here
-function ifOneIsRolled(roll) {
+function rollSumTotalIfNotOne(roll) {
   if (roll === 1) {
     return turn = 0;
   } else {
@@ -30,12 +40,38 @@ function ifOneIsRolled(roll) {
   }
 };
 
+// function to sum total and turn total
+
+PlayerInGame.prototype.turnToFinalTotal = function(turnTotal) {
+  this.total += turnTotal;
+  console.log(this.total);
+}
 
 // User Interface Logic
 $(document).ready(function() {
   $("button#roll").click(function() {
     var roll = diceRoll();
-    var turnTotal = ifOneIsRolled(roll);
+    var turnTotal = rollSumTotalIfNotOne(roll);
     console.log(roll, turnTotal);
+  });
+  $("button#hold").click(function() {
+
+  });
+  $("button#newGame").click(function(event) {
+    event.preventDefault();
+    $("button#newGame").hide();
+    var game = new Game();
+    $("form.playerField").show();
+  });
+  $("form.playerField").submit(function(event) {
+    event.preventDefault();
+    var player1 = $("input#playerName1").val();
+    var player2 = $("input#playerName2").val();
+    var playerInGame = new PlayerInGame(player1);
+    $("button#newGame").hide();
+    $("form.playerField").hide();
+    $("div.dieDisplay").show();
+    console.log(playerInGame);
+    console.log(player1, player2);
   });
 });
