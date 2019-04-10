656b2c716ae16814a2e82a829d58a88f96ad0aee
Lindsey Baker and Stuart McKay <lindsey@>
2019-04-10 16:02:12 -0700

    add hold button functionality to be able to take turn total and summand it with players running total

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,21 @@
 //Business Logic
 function diceRoll() {
   return Math.floor( Math.random() * 6 ) +1; // return a random value between [1, 6]
-}
+};
 
 // makes a new Game
 function Game() {
   this.players = [],
   this.currentId = 0
-}
+};
 
 // Adds a player to the game
-function PlayerInGame(name, total, turnTotal) {
+function PlayerInGame(name) {
   this.name = name,
-  this.total = 0,
-  this.turnTotal = turnTotal
-}
-
-// Assigns each new player a unique Id
-Game.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-// Adds new players to the Game
-Game.prototype.addPlayer = function(playerInGame) {
-  playerInGame.id = this.assignId();
-  this.players.push(playerInGame);
-}
+  this.total = 0
+};
 
 // funtion to sum rolls per turn resetting if one is rolled
-
 var turn = 0;
 function rollSumTotalIfNotOne(roll) {
   if (roll === 1) {
@@ -40,38 +26,64 @@ function rollSumTotalIfNotOne(roll) {
   }
 };
 
-// function to sum total and turn total
+// Assigns each new player a unique Id
+Game.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
 
-PlayerInGame.prototype.turnToFinalTotal = function(turnTotal) {
-  this.total += turnTotal;
-  console.log(this.total);
+// Adds new players to the Game
+Game.prototype.addPlayer = function(playerInGame) {
+  playerInGame.id = this.assignId();
+  this.players.push(playerInGame);
+};
+
+// Finds player is array and adds turn total to player total
+Game.prototype.findPlayer = function(name) {
+  for (var i=0; i< this.players.length; i++) {
+      if (this.players[i].name == name) {
+        return this.players[i];
+      }
+    };
+  return false;
 }
+
+// function to sum total and turn total
+PlayerInGame.prototype.turnToFinalTotal = function(turnTotals) {
+  this.total += turnTotals;
+  console.log(this.total);
+};
 
 // User Interface Logic
 $(document).ready(function() {
-  $("button#roll").click(function() {
-    var roll = diceRoll();
-    var turnTotal = rollSumTotalIfNotOne(roll);
-    console.log(roll, turnTotal);
-  });
-  $("button#hold").click(function() {
-
-  });
+  var game = new Game();
+  var turnTotal;
+  var playerInGame;
   $("button#newGame").click(function(event) {
     event.preventDefault();
     $("button#newGame").hide();
-    var game = new Game();
     $("form.playerField").show();
   });
   $("form.playerField").submit(function(event) {
     event.preventDefault();
     var player1 = $("input#playerName1").val();
     var player2 = $("input#playerName2").val();
-    var playerInGame = new PlayerInGame(player1);
+    playerInGame = new PlayerInGame(player1);
+    game.addPlayer(playerInGame);
+    console.log(game);
     $("button#newGame").hide();
     $("form.playerField").hide();
     $("div.dieDisplay").show();
     console.log(playerInGame);
     console.log(player1, player2);
+  });
+  $("button#roll").click(function() {
+    var roll = diceRoll();
+    turnTotal = rollSumTotalIfNotOne(roll);
+    console.log(roll, turnTotal);
+  });
+  $("button#hold").click(function() {
+    console.log(playerInGame)
+    playerInGame.turnToFinalTotal(turnTotal);
   });
 });
